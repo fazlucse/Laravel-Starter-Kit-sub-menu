33cf605efeeb77d41e61d96ad31171bdb0cf0785
fazluniloy <fazlucse123@gmail.com>
2026-03-13 17:25:54 +0600

    update seeder

--- database/seeders/InfoMasterSeeder.php
+++ database/seeders/InfoMasterSeeder.php
@@ -15,16 +15,7 @@ class InfoMasterSeeder extends Seeder
         // 🌍 Countries
         $countries = [
             ['name' => 'Bangladesh', 'code' => 'BD', 'description' => 'Country in South Asia'],
-            ['name' => 'India', 'code' => 'IN', 'description' => 'Country in South Asia'],
-            ['name' => 'Nepal', 'code' => 'NP', 'description' => 'Landlocked country in South Asia'],
-            ['name' => 'Bhutan', 'code' => 'BT', 'description' => 'Mountainous kingdom in the Himalayas'],
-            ['name' => 'Sri Lanka', 'code' => 'LK', 'description' => 'Island nation in South Asia'],
-            ['name' => 'Pakistan', 'code' => 'PK', 'description' => 'Country in South Asia'],
-            ['name' => 'United States', 'code' => 'US', 'description' => 'Country in North America'],
-            ['name' => 'Canada', 'code' => 'CA', 'description' => 'Country in North America'],
-            ['name' => 'United Kingdom', 'code' => 'UK', 'description' => 'Country in Europe'],
-            ['name' => 'Australia', 'code' => 'AU', 'description' => 'Country and continent in Oceania'],
-        ];
+          ];
 
         $countryIds = [];
         foreach ($countries as $country) {
@@ -61,18 +52,6 @@ class InfoMasterSeeder extends Seeder
             ['name' => 'Noakhali', 'code' => 'NKL', 'country' => 'Bangladesh', 'description' => 'City in southeastern Bangladesh'],
             ['name' => 'Bhola', 'code' => 'BHO', 'country' => 'Bangladesh', 'description' => 'Island city in southern Bangladesh'],
             ['name' => 'Dinajpur', 'code' => 'DNP', 'country' => 'Bangladesh', 'description' => 'City in northwestern Bangladesh'],
-
-            // 🌍 Some other cities
-            ['name' => 'Delhi', 'code' => 'DEL', 'country' => 'India', 'description' => 'Capital of India'],
-            ['name' => 'Mumbai', 'code' => 'BOM', 'country' => 'India', 'description' => 'Financial capital of India'],
-            ['name' => 'Kathmandu', 'code' => 'KTM', 'country' => 'Nepal', 'description' => 'Capital of Nepal'],
-            ['name' => 'Thimphu', 'code' => 'THI', 'country' => 'Bhutan', 'description' => 'Capital of Bhutan'],
-            ['name' => 'Colombo', 'code' => 'CMB', 'country' => 'Sri Lanka', 'description' => 'Commercial capital of Sri Lanka'],
-            ['name' => 'Karachi', 'code' => 'KHI', 'country' => 'Pakistan', 'description' => 'Largest city of Pakistan'],
-            ['name' => 'New York', 'code' => 'NYC', 'country' => 'United States', 'description' => 'City in USA'],
-            ['name' => 'Toronto', 'code' => 'YYZ', 'country' => 'Canada', 'description' => 'Largest city of Canada'],
-            ['name' => 'London', 'code' => 'LON', 'country' => 'United Kingdom', 'description' => 'Capital of the UK'],
-            ['name' => 'Sydney', 'code' => 'SYD', 'country' => 'Australia', 'description' => 'City in Australia'],
         ];
         $designations = [
             ['name' => 'Chief Executive Officer (CEO)', 'description' => 'Responsible for overall company direction and leadership.'],
