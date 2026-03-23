4aef1a474ea75be6a01375b3d6bccf6e54ceee69
fazluniloy <fazlucse123@gmail.com>
2026-03-23 17:36:11 +0600

    add seeder

--- database/migrations/2026_02_02_173118_create_payrolls_table.php
+++ database/migrations/2026_02_02_173118_create_payrolls_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
                     $blueprint->date('month')->index();
 
                     // Earnings (Calculated)
-                    $blueprint->double('gross_salary_calculated');
+                    $blueprint->double('gross_salary_calculated')->default(0);
                     $blueprint->double('basic_salary')->default(0);
                     $blueprint->double('house_rent')->default(0);
                     $blueprint->double('medical_allowance')->default(0);
@@ -31,44 +31,44 @@ return new class extends Migration
 
                     // Deductions
                     $blueprint->double('tax_deduction')->default(0);
-                    $blueprint->double('ips_deduction')->unsigned();
-                    $blueprint->double('ipd_deduction')->unsigned();
+                    $blueprint->double('ips_deduction')->unsigned()->default(0);
+                    $blueprint->double('ipd_deduction')->unsigned()->default(0);
                     $blueprint->double('loan_deduction')->default(0);
-                    $blueprint->double('pf_loan_deduction')->unsigned();
+                    $blueprint->double('pf_loan_deduction')->unsigned()->default(0);
                     $blueprint->double('mobile_deduction')->default(0);
                     $blueprint->double('training_deduction')->default(0);
-                    $blueprint->double('lwp_deduction');
-                    $blueprint->double('others_deduction');
-                    $blueprint->double('pf_deduction')->index();
-                    $blueprint->double('lb_deduction');
-                    $blueprint->double('canteen_bill_deduction');
-                    $blueprint->double('cafe_bill_deduction');
+                    $blueprint->double('lwp_deduction')->default(0);
+                    $blueprint->double('others_deduction')->default(0);
+                    $blueprint->double('pf_deduction')->default(0)->index();
+                    $blueprint->double('lb_deduction')->default(0);
+                    $blueprint->double('canteen_bill_deduction')->default(0);
+                    $blueprint->double('cafe_bill_deduction')->default(0);
 
                     // Final Payments
                     $blueprint->double('bank_amount')->default(0);
                     $blueprint->double('cash_amount')->default(0);
 
                     // Employee Snapshot (De-normalized for history)
-                    $blueprint->string('emp_id', 50);
-                    $blueprint->string('emp_name', 50);
-                    $blueprint->unsignedBigInteger('com_id')->index();
-                    $blueprint->string('com_name', 100);
-                    $blueprint->unsignedBigInteger('division_id')->index();
-                    $blueprint->string('division_name', 50)->index();
-                    $blueprint->unsignedBigInteger('department_id');
-                    $blueprint->string('department_name', 50);
-                    $blueprint->unsignedBigInteger('designation_id');
-                    $blueprint->string('designation_name', 50);
-                    $blueprint->date('joining_date');
+                    $blueprint->string('emp_id', 50)->nullable();
+                    $blueprint->string('emp_name', 50)->nullable();
+                    $blueprint->unsignedBigInteger('com_id')->default(0)->index();
+                    $blueprint->string('com_name', 100)->nullable();
+                    $blueprint->unsignedBigInteger('division_id')->nullable()->index();
+                    $blueprint->string('division_name', 50)->nullable()->index();
+                    $blueprint->unsignedBigInteger('department_id')->default(0);
+                    $blueprint->string('department_name', 50)->nullable();
+                    $blueprint->unsignedBigInteger('designation_id')->default(0);
+                    $blueprint->string('designation_name', 50)->nullable();
+                    $blueprint->date('joining_date')->nullable();
 
                     // System Meta
-                    $blueprint->unsignedBigInteger('entryby');
-                    $blueprint->datetime('entrytime');
+                    $blueprint->unsignedBigInteger('entryby')->nullable();
+                    $blueprint->datetime('entrytime')->nullable();
                     $blueprint->tinyInteger('locked')->unsigned()->default(0)->index();
-                    $blueprint->date('exchange_date');
-                    $blueprint->unsignedBigInteger('entry_by_dept');
-                    $blueprint->double('other_advance')->unsigned();
-                    $blueprint->tinyInteger('gsc_flag')->unsigned();
+                    $blueprint->date('exchange_date')->nullable();
+                    $blueprint->unsignedBigInteger('entry_by_dept')->default(0);
+                    $blueprint->double('other_advance')->unsigned()->default(0);
+                    $blueprint->tinyInteger('gsc_flag')->unsigned()->default(0);
 
                     $blueprint->timestamps();
                     // indexing
